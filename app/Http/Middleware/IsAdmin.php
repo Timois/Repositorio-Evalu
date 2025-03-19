d2f85cffa58e18620e2ae50e87dc35bb8ecaa14e
Timois <sis838timodruid@gmail.com>
2025-03-19 19:28:57 -0400

    add middlewares for roles

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $admin = auth('persona')->user();
+        if($admin && $admin->role == 'admin'){
+            return $next($request);
+        }else{
+            return response()->json(['message' => 'Tu no eres admin'], 403);
+        }
+    }
+}
